1659ffd51c8f98f255cdd5d24a4fe2ce4e329e91
Gabriele Gonçalves Vieira <gabrielevieira011@gmail.com>
2024-01-05 12:08:04 -0300

    feat: initial config and title component

---
+++ src/components/form/form.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native'
+import ResultImc from "./resultImc/resultImc"
+import styles from "./form/formStyles";
+
+export default function Form() {
+    return (
+        <VIew>
+
+        </VIew>
+    )
+}
---
+++ src/components/title/title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./titleStyle";
+
+
+
+export default function Title() {
+    return (
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}> IMC Fit Calc</Text>
+        </View>
+    )
+}
